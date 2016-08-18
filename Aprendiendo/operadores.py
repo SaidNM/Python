1e87b1b8b7bd8214205c143f0add59887f06a16b
saidnm <saidnm044@hotmail.com>
2016-08-17 23:58:05 -0600

    Primer dia con python

---
+++ Aprendiendo/operadores.py
@@ -0,0 +1,26 @@
+""" e = 0.5  se asgina un tipo int
+e = 0.5L  se asigna un tipo long
+real = 0.567  se asigna un tipo float 
+real = 0.56e3  se utiliza e como un operador equivalente a exponente 10, es decir x10^n
+print (real) """
+
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#suma
+print (a+b)
+# resta
+print (c-a)
+# Multiplicacion
+print (d*c)
+#exponente
+print (c**2)
+#division
+print (int(c/a))
+#division entera
+print (c/a)
+#modulo
+print (7%3)
+
---
+++ Aprendiendo/oplogybool.py
@@ -0,0 +1,33 @@
+#comillas simples
+cads = 'Texto entre comillas simples\n'
+#comillas dobles
+cadd = "\tTexto entre comillas dobles"
+cadc = """ Texto entre comillas triples
+sin caracteres
+de escape"""
+
+print (type(cads)) #type se utiliza para saber que tipo de dato es
+print (type(cadd))
+print (cads , cadd) #concatenacion de texto
+print (cadc)
+
+cad = 'Cadena' * 4
+cad1= 'hola'
+cad2 = 'mundo'
+print (cad)
+print (cad1 + cad2) 
+
+#valores booleanos
+bT = True
+bf = False
+
+#operadores logicos
+bAnd = True and False #operador and
+bOr = True or False #operador or
+bNot = not True #operador not
+
+print(bAnd)
+print(bOr)
+print(bNot)
+
+
